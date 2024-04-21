022ad7672f222c5579c260fab2e5fc2c72125502
shayefordring <slc485@nau.edu>
2024-04-21 09:18:12 -0700

    bus changes

--- nau_stops.js
+++ nau_stops.js
@@ -7,18 +7,18 @@ const tiles = L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
 
 // Jacks line (colored yellow)
 var jacksList = [
-    marker1 = L.marker([35.17814395416947, -111.65254952946732]).bindPopup("Pine Ridge<br/>Next Bus: 7m<br/>Traffic Conditions: Medium<br/>There May be Standing Room Only!"),
-    marker2 = L.marker([35.17553940892971, -111.65634217301437]).bindPopup("Forestry</br>Next Bus: 3m<br/>Traffic Conditions: Light<br/>Bus is Not Full!"),
+    marker1 = L.marker([35.17814395416947, -111.65254952946732]).bindPopup("Pine Ridge<br/>Next Bus: 1m<br/>Traffic Conditions: Medium<br/>There May be Standing Room Only!"),
+    marker2 = L.marker([35.17553940892971, -111.65634217301437]).bindPopup("Forestry</br>Next Bus: 1m<br/>Traffic Conditions: Light<br/>Bus is Not Full!"),
     marker3 = L.marker([35.176758374019876, -111.65918924264419]).bindPopup("South Commuter<br/>Next Bus: 2m</br>Traffic Conditions: Light<br/>Bus is Not Full!"),
-    marker4 = L.marker([35.178735877081905, -111.65821828298084]).bindPopup("SBS<br/>Next Bus: 3m<br/>Traffic Conditions: Heavy<br/>Standing Room Only<br/>Bus May be Full!"),
+    marker4 = L.marker([35.178735877081905, -111.65821828298084]).bindPopup("SBS<br/>Next Bus: 2m<br/>Traffic Conditions: Heavy<br/>Standing Room Only<br/>Bus May be Full!"),
     marker5 = L.marker([35.18287403636424, -111.65694648325938]).bindPopup("Campus Heights<br/>Next Bus: 4m<br/>Traffic Conditions: Medium<br/>There May be Standing Room Only!"),
-    marker6 = L.marker([35.18639420920059, -111.65698458227844]).bindPopup("Tinsley Hall<br/>Next Bus: 7m<br/>Traffic Conditions: Medium<br/>There May be Standing Room Only!"),
-    marker7 = L.marker([35.18899587749302, -111.65712033616087]).bindPopup("Knoles Union<br/>Next Bus: 3m<br/>Traffic Conditions: Heavy<br/>Standing Room Only<br/>Bus May be Full!"),
-    marker8 = L.marker([35.19235402917077, -111.65670191155453]).bindPopup("Old Main<br/>Next Bus: 5m</br>Traffic Conditions: Light<br/>Bus is Not Full!"),
-    marker9 = L.marker([35.19347523316377, -111.65288598717972]).bindPopup("Centennial<br/>Next Bus: 4m</br>Traffic Conditions: Light<br/>Bus is Not Full!"),
+    marker6 = L.marker([35.18639420920059, -111.65698458227844]).bindPopup("Tinsley Hall<br/>Next Bus: 4m<br/>Traffic Conditions: Medium<br/>There May be Standing Room Only!"),
+    marker7 = L.marker([35.18899587749302, -111.65712033616087]).bindPopup("Knoles Union<br/>Next Bus: 5m<br/>Traffic Conditions: Heavy<br/>Standing Room Only<br/>Bus May be Full!"),
+    marker8 = L.marker([35.19235402917077, -111.65670191155453]).bindPopup("Old Main<br/>Next Bus: 6m</br>Traffic Conditions: Light<br/>Bus is Not Full!"),
+    marker9 = L.marker([35.19347523316377, -111.65288598717972]).bindPopup("Centennial<br/>Next Bus: 6m</br>Traffic Conditions: Light<br/>Bus is Not Full!"),
     marker10 = L.marker([35.19128664929352, -111.65327624200957]).bindPopup("Bio Science<br/>Next Bus: 7m<br/>Traffic Conditions: Medium<br/>There May be Standing Room Only!"),
-    marker11 = L.marker([35.18822988441976, -111.65441093531864]).bindPopup("Transit Spine Union<br/>Next Bus: 3m<br/>Traffic Conditions: Heavy<br/>Standing Room Only<br/>Bus May be Full!"),
-    marker12 = L.marker([35.17997067729946, -111.65097718626353]).bindPopup("Skydome<br/>Next Bus: 2m</br>Traffic Conditions: Light<br/>Bus is Not Full!"),
+    marker11 = L.marker([35.18822988441976, -111.65441093531864]).bindPopup("Transit Spine Union<br/>Next Bus: 7m<br/>Traffic Conditions: Heavy<br/>Standing Room Only<br/>Bus May be Full!"),
+    marker12 = L.marker([35.17997067729946, -111.65097718626353]).bindPopup("Skydome<br/>Next Bus: 7m</br>Traffic Conditions: Light<br/>Bus is Not Full!"),
     path1 = L.geoJSON({"type":"FeatureCollection","features":[{"type":"Feature","properties":{},"geometry":{"coordinates":[[-111.65258369039681,35.178039946432875],[-111.65388758015699,35.177446512583685],[-111.65453952503728,35.17698629559639],[-111.65578414708114,35.176059798231734],
     [-111.65643609344632,35.175411848115516],[-111.6568806013191,35.17526045744731],[-111.65727324994033,35.175254401814186],[-111.6577399832065,35.17533918062988],[-111.65804373025281,35.17548451553618],[-111.65964507824428,35.17722843461071],[-111.65963766977954,35.17743432086681],[-111.65948209202399,35.17767653932357],[-111.65936355659136,35.178033810230815],[-111.65942282430767,35.178324470148794],[-111.65825228633953,35.17863934793198],[-111.65768183456939,35.17904505742557],[-111.65835251719253,35.17960655225947],[-111.65850068648372,35.17994564883497],[-111.65841919337359,35.18024235717863],[-111.65820434790174,35.180472456781075],[-111.65716633262704,35.18100702868844],
     [-111.6569366702261,35.181212905373926],[-111.6568181347935,35.18141878153794],[-111.6567509988958,35.181690499182494],[-111.65674359043149,35.18220518558999],[-111.65678063371291,35.1823603333434],[-111.65700288764909,35.182705473508804],[-111.6570028881208,35.18442499726797],[-111.6570320295181,35.18476731604021],[-111.65713574802207,35.1849368537393],[-111.65729873424192,35.18524565363968],[-111.65729589304567,35.18549813103316],[-111.65709586450289,35.18585536755988],[-111.65702901221913,35.186097705668104],[-111.65702160375481,35.1877022184582],[-111.65707821805113,35.188113592136304],[-111.65704117572822,35.18874933050522],[-111.6570777813646,35.190173942399596],[-111.65706296476986,35.19058499709607],
@@ -32,18 +32,18 @@ jacksList.forEach((jacksMarker) => {if (jacksMarker !== path1) {jacksMarker.setI
 
 // Louie line (colored blue)
 var louieList = [
-    marker13 = L.marker([35.177156448705965, -111.65408819716697]).bindPopup("Mconnell Hall\rNext Bus in: 5m\rGood to Go!"),
-    marker14 = L.marker([35.18013854253613, -111.6507922442267]).bindPopup("San Francisco"),
-    marker15 = L.marker([35.1885705573119, -111.65414904852096]).bindPopup("Bookstore"),
-    marker16 = L.marker([35.191440816484686, -111.6531363084861]).bindPopup("Bio Science"),
-    marker17 = L.marker([35.19367638627814, -111.6532535464701]).bindPopup("Print Shop"),
-    marker18 = L.marker([35.19254943729683, -111.65673864517328]).bindPopup("Geology"),
-    marker19 = L.marker([35.18844597170686, -111.65718974330227]).bindPopup("Ardery Auditorium"),
-    marker20 = L.marker([35.18532150407906, -111.6574424695362]).bindPopup("Applied Research & Development"),
-    marker21 = L.marker([35.1823840427844, -111.65694220721862]).bindPopup("Gabaldon"),
-    marker22 = L.marker([35.178587071368476, -111.65818647202283]).bindPopup("SBS"),
-    marker23 = L.marker([35.17690009265183, -111.65951600604863]).bindPopup("South Commuter"),
-    marker24 = L.marker([35.17528211188283, -111.656502147603]).bindPopup("South Village"),
+    marker13 = L.marker([35.177156448705965, -111.65408819716697]).bindPopup("Mconnell Hall<br/>Next Bus: 1m<br/>Traffic Conditions: Medium<br/>There May be Standing Room Only!"),
+    marker14 = L.marker([35.18013854253613, -111.6507922442267]).bindPopup("San Francisco<br/>Next Bus: 2m</br>Traffic Conditions: Light<br/>Bus is Not Full!"),
+    marker15 = L.marker([35.1885705573119, -111.65414904852096]).bindPopup("Bookstore<br/>Next Bus: 2m<br/>Traffic Conditions: Heavy<br/>Standing Room Only<br/>Bus May be Full!"),
+    marker16 = L.marker([35.191440816484686, -111.6531363084861]).bindPopup("Bio Science<br/>Next Bus: 4m<br/>Traffic Conditions: Medium<br/>There May be Standing Room Only!"),
+    marker17 = L.marker([35.19367638627814, -111.6532535464701]).bindPopup("Print Shop<br/>Next Bus: 5m</br>Traffic Conditions: Light<br/>Bus is Not Full!"),
+    marker18 = L.marker([35.19254943729683, -111.65673864517328]).bindPopup("Geology<br/>Next Bus: 5m</br>Traffic Conditions: Light<br/>Bus is Not Full!"),
+    marker19 = L.marker([35.18844597170686, -111.65718974330227]).bindPopup("Ardery Auditorium<br/>Next Bus: 5m<br/>Traffic Conditions: Heavy<br/>Standing Room Only<br/>Bus May be Full!"),
+    marker20 = L.marker([35.18532150407906, -111.6574424695362]).bindPopup("Applied Research & Development<br/>Next Bus: 6m<br/>Traffic Conditions: Heavy<br/>Standing Room Only<br/>Bus May be Full!"),
+    marker21 = L.marker([35.1823840427844, -111.65694220721862]).bindPopup("Gabaldon<br/>Next Bus: 6m<br/>Traffic Conditions: Medium<br/>There May be Standing Room Only!"),
+    marker22 = L.marker([35.178587071368476, -111.65818647202283]).bindPopup("SBS<br/>Next Bus: 6m<br/>Traffic Conditions: Heavy<br/>Standing Room Only<br/>Bus May be Full!"),
+    marker23 = L.marker([35.17690009265183, -111.65951600604863]).bindPopup("South Commuter<br/>Next Bus: 7m</br>Traffic Conditions: Light<br/>Bus is Not Full!"),
+    marker24 = L.marker([35.17528211188283, -111.656502147603]).bindPopup("South Village<br/>Next Bus: 7m</br>Traffic Conditions: Light<br/>Bus is Not Full!"),
 ];
 
 louieList.forEach((louieMarker) => {if (louieMarker !== path2) {louieMarker.setIcon(L.divIcon({className: "dot-blue"}))}});

561e298c108cae259ac459423bcaa434a57cdddb
dragifire <charlieshaskins@gmail.com>
2024-04-20 20:51:38 -0700

    a

--- nau_stops.js
+++ nau_stops.js
@@ -5,7 +5,7 @@ const tiles = L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
     }).addTo(map);
 
 // Jacks line (colored yellow)
-const jacks = L.layerGroup([
+const jacksList = [
     marker1 = L.marker([35.17814395416947, -111.65254952946732]).bindPopup("Pine Ridge"),
     marker2 = L.marker([35.17553940892971, -111.65634217301437]).bindPopup("Forestry"),
     marker3 = L.marker([35.176758374019876, -111.65918924264419]).bindPopup("South Commuter"),
@@ -18,14 +18,14 @@ const jacks = L.layerGroup([
     marker10 = L.marker([35.19128664929352, -111.65327624200957]).bindPopup("Bio Science"),
     marker11 = L.marker([35.18822988441976, -111.65441093531864]).bindPopup("Transit Spine Union"),
     marker12 = L.marker([35.17997067729946, -111.65097718626353]).bindPopup("Skydome"),
-]);
+];
 
-for (marker in jacks) {
+for (marker of jacksList) {
     marker.setIcon(L.divIcon({classname: "dot-blue"}))
 };
 
 // Louie line (colored blue)
-const louie = L.layerGroup([
+const louieList = [
     marker13 = L.marker([35.177156448705965, -111.65408819716697]).bindPopup("Mconnell Hall"),
     marker14 = L.marker([35.18013854253613, -111.6507922442267]).bindPopup("San Francisco"),
     marker15 = L.marker([35.1885705573119, -111.65414904852096]).bindPopup("Bookstore"),
@@ -38,11 +38,14 @@ const louie = L.layerGroup([
     marker22 = L.marker([35.178587071368476, -111.65818647202283]).bindPopup("SBS"),
     marker23 = L.marker([35.17690009265183, -111.65951600604863]).bindPopup("South Commuter"),
     marker24 = L.marker([35.17528211188283, -111.656502147603]).bindPopup("South Village"),
-]);
+];
 
-for (marker in louie) {
+for (marker in louieList) {
     marker.setIcon(L.divIcon({classname: "dot-yellow"}))
 };
+
+const louie = L.layerGroup(louieList);
+const jacks = L.layerGroup(jacksList);
 
 const overlays = {
     "Louie" : louie,
